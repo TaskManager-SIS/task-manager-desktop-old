908553e4ba4c4c3aca5ba5d884092efab495d393
медовый <100449745+Zvoruna@users.noreply.github.com>
2023-03-29 18:21:25 -0300

    Commit Taskmanager-Desktop 1.0
    Versão Inicial
    - Em progresso: Cadastro

---
+++ TaskManager-Desktop/Usuario/Cadastro.Designer.cs
@@ -0,0 +1,86 @@
+﻿using System.Runtime.InteropServices;
+using System;
+
+namespace TaskManager_Desktop
+{
+    partial class Cadastro
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Cadastro));
+            this.Bloco = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Bloco
+            // 
+            this.Bloco.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.Bloco.BackColor = System.Drawing.Color.White;
+            this.Bloco.Location = new System.Drawing.Point(314, 155);
+            this.Bloco.Margin = new System.Windows.Forms.Padding(3, 10, 3, 3);
+            this.Bloco.Name = "Bloco";
+            this.Bloco.Size = new System.Drawing.Size(626, 469);
+            this.Bloco.TabIndex = 0;
+            this.Bloco.Paint += new System.Windows.Forms.PaintEventHandler(this.Bloco_Paint);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.pictureBox1.Image = global::TaskManager_Desktop.Properties.Resources.Group_24;
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(407, 12);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 3, 3, 10);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(413, 76);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Cadastro
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(56)))), ((int)(((byte)(79)))));
+            this.ClientSize = new System.Drawing.Size(1260, 677);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Bloco);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Cadastro";
+            this.Text = "Cadastro";
+            this.Load += new System.EventHandler(this.Cadastro_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel Bloco;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ TaskManager-Desktop/Usuario/Cadastro.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TaskManager_Desktop
+{
+    public partial class Cadastro : Form
+    {
+        //round corners
+
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn
+        (
+            int nLeftRect,     // x-coordinate of upper-left corner
+            int nTopRect,      // y-coordinate of upper-left corner
+            int nRightRect,    // x-coordinate of lower-right corner
+            int nBottomRect,   // y-coordinate of lower-right corner
+            int nWidthEllipse, // width of ellipse
+            int nHeightEllipse // height of ellipse
+        );
+
+        public Cadastro()
+        {
+            InitializeComponent();
+        }
+
+        private void Bloco_Paint(object sender, PaintEventArgs e)
+        {
+            //centralizado
+            Bloco.Left = (this.ClientSize.Width - Bloco.Width) / 2;
+            //Bloco.Top = (this.ClientSize.Height - Bloco.Height) / 2;
+            
+            //Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 20, 20));
+            Bloco.Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, Bloco.Width, Bloco.Height, 20, 20));
+        }
+
+        /* Caso Formborder -> Resize */
+        private void Cadastro_SizeChanged(object sender, EventArgs e)
+        {
+            CenterControlInParent(Bloco);
+        }
+        private void CenterControlInParent(Control ctrlToCenter)
+        {
+            ctrlToCenter.Left = (ctrlToCenter.Parent.Width - ctrlToCenter.Width) / 2;
+            //ctrlToCenter.Top = (ctrlToCenter.Parent.Height - ctrlToCenter.Height) / 2;
+        } 
+
+        private void Cadastro_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
